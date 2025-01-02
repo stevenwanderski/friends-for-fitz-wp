3c60a5947d7a2b086cde1f9926d53b6d80eaef09
Steven Wanderski <steven.wanderski@gmail.com>
2025-01-02 17:29:51 -0600

    Adds contact form

--- wp-content/themes/friends-for-fitz/page-contact.php
+++ wp-content/themes/friends-for-fitz/page-contact.php
@@ -19,4 +19,67 @@
   )
 ); ?>
 
+<div class="wrap">
+  <div class="container-inner">
+    <div class="main grid gap-[64px] lg:grid-cols-2 lg:gap-0">
+      <div class="content lg:pr-[72px] lg:border-r lg:border-color-gray-lt">
+        <div class="header max-w-[600px] mb-[36px]">
+          <h2 class="mb-[24px]">Send us a message.</h2>
+
+          <p class="text-medium">
+            We'd love to hear from you! Reach out with any questions, feedback, or to learn more about how you can get involved.
+          </p>
+        </div>
+
+        <?php echo do_shortcode('[fluentform id="1"]'); ?>
+      </div>
+
+      <div class="info flex flex-col gap-[72px] lg:pl-[72px]">
+        <div class="item">
+          <img
+            src="<?php echo get_bloginfo('template_directory') . '/resources/images/icon-email.svg' ?>"
+            class="mb-[16px]"
+          />
+
+          <div class="label font-bold text-[20px] mb-[4px]">
+            Email
+          </div>
+
+          <a href="mailto:friends4fitz@gmail.com" class="value text-[20px]">
+            friends4fitz@gmail.com
+          </a>
+        </div>
+
+        <div class="item">
+          <img
+            src="<?php echo get_bloginfo('template_directory') . '/resources/images/icon-phone.svg' ?>"
+            class="mb-[16px]"
+          />
+
+          <div class="item-group mb-[32px]">
+            <div class="label font-bold text-[20px] mb-[4px]">
+              Erin Cotter
+            </div>
+
+            <a href="tel:773-879-1060" class="value text-[20px]">
+              773-879-1060
+            </a>
+          </div>
+
+          <div class="item-group mb-[32px]">
+            <div class="label font-bold text-[20px] mb-[4px]">
+              Katie Ramsden
+            </div>
+
+            <a href="tel:773-879-1058" class="value text-[20px]">
+              773-879-1058
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <?php get_footer(); ?>
